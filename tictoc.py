4eec6643dd8e0b652b83ae329394e7d6cb7581fa
Alon Jacobson <alon.jacobson@tufts.edu>
2020-04-27 16:37:25 -0400

    change tictoc.py so time module doesn't get imported from it

--- tictoc.py
+++ tictoc.py
@@ -1,4 +1,4 @@
-from time import time
+from time import time as _time
 
 _start_times = {}
 
@@ -6,7 +6,7 @@ _start_times = {}
 def tic(name=None, say_name=False):
     if say_name:
         print(name)
-    _start_times[name] = time()
+    _start_times[name] = _time()
     return _start_times[name]
 
 
@@ -14,7 +14,7 @@ def toc(name=None):
     if name not in _start_times:
         argument = repr(name) if name is None else ''
         raise RuntimeError(f"Error using toc: You must call tic({argument}) before calling toc({argument})")
-    dt = time() - _start_times[name]
+    dt = _time() - _start_times[name]
     if name is None:
         print(f"Elapsed time: {dt:.6f} seconds")
     else:
