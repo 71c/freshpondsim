9e5d90acf0d813e04bd8fb3b152810178f367976
Alon Jacobson <alon.jacobson@tufts.edu>
2020-07-04 20:10:21 -0400

    committing changes from loong time ago, and changed pipfile

--- simulation_2.py
+++ simulation_2.py
@@ -5,6 +5,7 @@ from tictoc import *
 import matplotlib.pyplot as plt
 from simulation import plot_pace_vs_intersection_direction
 import cProfile
+from scipy.stats import ttest_rel
 
 
 def get_indepenent_velocities_and_distances_func(vfunc, dfunc):
@@ -72,8 +73,8 @@ def get_simple_sim_multiple_mile_time(distance, dt, entrance_rate, mile_times,
 
     return FreshPondSim(distance,
                         0,
-                        dt,
-                        [0, 0.3*distance, 0.5*distance, 0.7*distance], [1, 1, 1, 1],
+                        dt, [0, 0.3 * distance, 0.5 * distance, 0.7 * distance],
+                        [1, 1, 1, 1],
                         get_vdist,
                         rate_func,
                         entrance_rate_integral=rate_integral_func,
@@ -140,12 +141,13 @@ def plot_pace_vs_intersection_direction_simple_multiple_mile_time(
     # start time of the observers
     start_time = dt - max_observer_dt
 
-    people_counts = plot_pace_vs_intersection_direction(sim,
-                                        start_time=start_time,
-                                        start_pos=0,
-                                        observer_distance=observer_distance,
-                                        mile_times=mile_times,
-                                        n_reps=n_reps)
+    people_counts = plot_pace_vs_intersection_direction(
+        sim,
+        start_time=start_time,
+        start_pos=0,
+        observer_distance=observer_distance,
+        mile_times=mile_times,
+        n_reps=n_reps)
 
     # cProfile.runctx('''plot_pace_vs_intersection_direction(sim,
     #                                     start_time=start_time,
@@ -157,11 +159,15 @@ def plot_pace_vs_intersection_direction_simple_multiple_mile_time(
     return mile_times, people_counts, sim, start_time
 
 
-def predict_true_averages(sim_dist, entrance_rate, mile_times_choices, mile_times_probs, distance_prop, observer_distance_prop, same_direction_prop, mile_times):
+def predict_true_averages(sim_dist, entrance_rate, mile_times_choices,
+                          mile_times_probs, distance_prop,
+                          observer_distance_prop, same_direction_prop,
+                          mile_times):
     tmp = entrance_rate * distance_prop * observer_distance_prop * sim_dist
     w = np.array(mile_times_choices).reshape(-1, 1)
     P = np.array(mile_times_probs).reshape(-1, 1)
-    nsametot = same_direction_prop * tmp * (abs(w - mile_times) * P).sum(axis=0) / np.sum(P)
+    nsametot = same_direction_prop * tmp * (abs(w - mile_times) *
+                                            P).sum(axis=0) / np.sum(P)
 
     mean_w = (w * P).sum() / P.sum()
 
@@ -172,8 +178,8 @@ def predict_true_averages(sim_dist, entrance_rate, mile_times_choices, mile_time
 def test_theory():
     sim_dist = 2.46
     entrance_rate = 2.2
-    mile_times_choices = [5,     10,     11,     9,   20,  23,  15,  18,  19, 21]
-    mile_times_probs =   [0.05, 0.05, 0.025, 0.025, 0.25, 0.2, 0.1, 0.1, 0.1, 0.1]
+    mile_times_choices = [5, 10, 11, 9, 20, 23, 15, 18, 19, 21]
+    mile_times_probs = [0.05, 0.05, 0.025, 0.025, 0.25, 0.2, 0.1, 0.1, 0.1, 0.1]
     distance_prop = 0.6
     observer_distance_prop = 1.0
     same_direction_prop = 0.5
@@ -190,16 +196,18 @@ def test_theory():
         same_direction_prop=same_direction_prop,
         max_observer_mile_time=max_observer_mile_time,
         mile_time_sample_interval=mile_time_sample_interval,
-        n_reps=1)
+        n_reps=10)
 
     # mile_times = np.arange(0.01, max_observer_mile_time,
-                           # mile_time_sample_interval)
+    # mile_time_sample_interval)
 
-    nsame, ndiff = predict_true_averages(sim_dist, entrance_rate, mile_times_choices, mile_times_probs, distance_prop, observer_distance_prop, same_direction_prop, mile_times)
+    nsame, ndiff = predict_true_averages(sim_dist, entrance_rate,
+                                         mile_times_choices, mile_times_probs,
+                                         distance_prop, observer_distance_prop,
+                                         same_direction_prop, mile_times)
     plt.plot(mile_times, nsame, '-', mile_times, ndiff, '-')
-    plt.legend(['num same', 'num diff', 'num same pred', 'num diff pred'], loc='best')
-
-
+    plt.legend(['num same', 'num diff', 'num same pred', 'num diff pred'],
+               loc='best')
 
     people_counts = list(people_counts)
     for i in range(400):
@@ -209,15 +217,102 @@ def test_theory():
     mean_count = np.mean(people_counts)
     std_count = np.std(people_counts, ddof=1)
     count_std_err = std_count / np.sqrt(len(people_counts))
-    expected_mean_count = entrance_rate * np.dot(mile_times_choices, mile_times_probs) * distance_prop * sim_dist
+    expected_mean_count = entrance_rate * np.dot(
+        mile_times_choices, mile_times_probs) * distance_prop * sim_dist
     print("People count mean:", mean_count)
     print("People count std:", std_count)
     print("People count mean standard error:", count_std_err)
     print("Expected mean count:", expected_mean_count)
 
 
+def test_prediction_stationary():
+    sim = get_simple_sim_multiple_mile_time(
+        2.46, 60 * 60 * 5, 2.2, [5, 10, 11, 9, 20, 23, 15, 18, 19, 21],
+        [0.05, 0.05, 0.025, 0.025, 0.25, 0.2, 0.1, 0.1, 0.1, 0.1], 2.0, 1.0)
+
+    p = FreshPondPedestrian(sim.dist_around,
+                            1.0,
+                            0,
+                            60 * 60 * 4,
+                            time_delta=36)
+
+    predicted_mean_w = 18.15
+    # predicted_mean_w = 20
+
+    preds = []
+    trues = []
+    for _ in range(40):
+        predicted_n_people = sim.n_people_saw(p) * predicted_mean_w * sim.dist_around / (p.end_time - p.start_time)
+
+        # true_n_people = sim.n_people(p.start_time)
+
+        true_n_people = (sim.n_people(p.start_time) + sim.n_people(p.end_time) + sim.n_people((p.start_time+p.end_time)/2))/3
+
+        preds.append(predicted_n_people)
+        trues.append(true_n_people)
+        sim.refresh_pedestrians()
+        print(predicted_n_people, true_n_people)
+    print(ttest_rel(preds, trues))
+
+    preds = np.array(preds)
+    trues = np.array(trues)
+    print(np.mean(preds - trues), np.std(preds - trues, ddof=1))
+
+    print(np.mean(trues), np.std(trues, ddof=1))
+    print(np.mean(preds), np.std(preds, ddof=1))
 
 
+def test_prediction_moving():
+    mile_times = np.array([5, 10, 11, 9, 20, 23, 15, 18, 19, 21])
+    mile_times_probs = np.array([0.05, 0.05, 0.025, 0.025, 0.25, 0.2, 0.1, 0.1, 0.1, 0.1])
+    sim = get_simple_sim_multiple_mile_time(
+        2.46, 60 * 60 * 5, 2.2, mile_times,
+        mile_times_probs, 2.0, 0.5)
+
+    p = FreshPondPedestrian(sim.dist_around,
+                            1.0,
+                            sim.dist_around,
+                            60 * 60 * 4,
+                            velocity=1/15)
+
+    predicted_mean_w = 18.15
+    # predicted_mean_w = 20
+
+    k = p.travel_distance/sim.dist_around
+
+    ewwobs = np.dot(abs(mile_times - (1/p.velocity)), mile_times_probs)
+    esum = np.dot(mile_times, mile_times_probs) + (1/p.velocity)
+
+    preds = []
+    trues = []
+    for _ in range(40):
+        n_same, n_diff = sim.intersection_directions_total(p)
+        n_tot = n_same + n_diff
+
+        if n_same == 0:
+            P = 0
+        else:
+            P = esum / (n_diff / n_same * ewwobs + esum)
+
+        print(n_tot, "n_tot")
+        # print(P, "P")
+
+        predicted_n_people = predicted_mean_w * n_tot / (k * (P * ewwobs + (1-P) * esum))
+
+        true_n_people = (sim.n_people(p.start_time) + sim.n_people(p.end_time) + sim.n_people((p.start_time+p.end_time)/2))/3
+
+        preds.append(predicted_n_people)
+        trues.append(true_n_people)
+        sim.refresh_pedestrians()
+        print(predicted_n_people, true_n_people)
+    print(ttest_rel(preds, trues))
+
+    preds = np.array(preds)
+    trues = np.array(trues)
+    print(np.mean(preds - trues), np.std(preds - trues, ddof=1))
+
+    print(np.mean(trues), np.std(trues, ddof=1))
+    print(np.mean(preds), np.std(preds, ddof=1))
 
 
 def main():
@@ -245,9 +340,14 @@ def main():
     #     n_reps=10)
     # toc()
 
-    test_theory()
+    # test_theory()
+    # plt.show()
 
-    plt.show()
+    # test_prediction_moving()
+    test_prediction_stationary()
+
+    # test_P_prediction()
+
 
 
 if __name__ == '__main__':
---
+++ speed_data.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def group_by(items, group_function=lambda x: x):
+    """
+    Groups items by a grouping function
+    parameters:
+        items: iterable containing things
+        group_function: function that gives the same result when called on two
+            items in the same group
+    returns: a dict where the keys are results of the function and values are
+        lists of items that when passed to group_function give that key
+    """
+    group_dict = {}
+    for item in items:
+        value = group_function(item)
+        if value in group_dict:
+            group_dict[value].append(item)
+        else:
+            group_dict[value] = [item]
+    return group_dict
+
+
+times_1 = [(5.99, 'bike'), (25.03, 'walk'), (17.65, 'walk'), (9.71, 'run'),
+           (21.73, 'walk'), (4.48, 'bike'), (13.12, 'run'), (12.43, 'walk'),
+           (19.12, 'walk'), (18.32, 'walk'), (26.70, 'walk'), (8.37, 'run')]
+distance_1 = 81 + 5 / 12
+
+times_2 = [(16.46, 'walk'), (10.46, 'run'), (10.33, 'run'), (9.88, 'bike'),
+           (21.42, 'walk'), (18.0, 'walk'), (13.86, 'run'), (7.69, 'run'),
+           (7.30, 'run'), (9.16, 'run'), (7.84, 'run'), (21.11, 'walk'),
+           (8.35, 'run'), (8.68, 'run'), (18.55, 'walk'), (7.67, 'run'),
+           (22.38, 'walk'), (27.08, 'walk'), (13.60, 'walk'), (8.69, 'run'),
+           (18.98, 'walk'), (10.21, 'run')]
+distance_2 = 80 + 10 / 12
+
+# minutes per mile
+paces_1 = [(88 * dt / distance_1, name) for dt, name in times_1]
+paces_2 = [(88 * dt / distance_2, name) for dt, name in times_2]
+
+# meters per second
+# paces_1 = [(0.3048 * distance_1 / dt, name) for dt, name in times_1]
+# paces_2 = [(0.3048 * distance_2 / dt, name) for dt, name in times_2]
+
+
+paces = paces_1 + paces_2
+paces_dict = {
+    k: [x[0] for x in v]
+    for k, v in group_by(paces, lambda x: x[1]).items()
+}
+
+# minutes per mile == 26.8224 / (meters per second)
+
+for name, values in paces_dict.items():
+
+    mean = np.mean(values)
+    std = np.std(values, ddof=1)
+    n = len(values)
+    mean_standard_error = std / np.sqrt(n)
+    print(f"{name}: n: {n}, mean: {mean:.4g} ± {mean_standard_error:.4g}, std: {std:.4g}")
+
+    plt.hist(values, bins='auto', density=True, histtype='step', label=name)
+plt.legend()
+
+plt.show()
